3853bda15768e73f6db361bbf2f8fab3b2165acd
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-04-21 20:10:53 -0700

    adds fetch send data lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email){
+    const bioData = {
+        name : name,
+        email : email
+    }
+    return fetch("http://localhost:3000/users", {
+        method:"POST",
+        headers:{
+            "Content-Type" : "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(bioData)
+    })
+    .then(res => res.json())
+    .then(function(data){document.body.innerHTML = data.id})
+    .catch(function(error){
+        alert("Bad request");
+        document.body.innerHTML += error.message
+    })
+}
